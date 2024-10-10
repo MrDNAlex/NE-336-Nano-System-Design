8d2465ea82df4ef996ea6cc5f8c6ccdf49e540b6
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2024-10-10 17:44:29 -0400

    Update to Newton Raphson

--- Scripts/Newton Raphson/NewtonRaphson.py
+++ Scripts/Newton Raphson/NewtonRaphson.py
@@ -1,16 +1,26 @@
 # Newton Raphson Method Formulation
-def NewtonRaphson (x0, f, fp, tolerance = 0.005, maxIteration = 100):
+def NewtonRaphson (x0, f, fp = None, tolerance = 0.005, maxIteration = 100):
     xold = x0
     xnew = 0
     count = 0
     error = 1000
+    delta = 10**(-6)
     
     xHistory = []
     xHistory.append(x0)
     errorHistory = []
     
     while (error > tolerance and count < maxIteration):
-        xnew = xold - f(xold)/fp(xold)
+        
+        fpVal = 1
+        
+        if (fp is None):
+            fpVal = (f(xold + delta) - f(xold)) / delta
+        else:
+            fpVal = fp(xold)
+        
+        
+        xnew = xold - f(xold)/fpVal
         error = abs((xnew - xold)/xnew)*100
         xHistory.append(xnew)
         errorHistory.append(error)
