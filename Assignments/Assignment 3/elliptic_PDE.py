40e7bbfe7d2edcbccc2d322a0e505ef0cb06829a
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2024-11-03 15:14:45 -0500

    Assignment 3 Complete

---
+++ Assignments/Assignment 3/elliptic_PDE.py
@@ -0,0 +1,140 @@
+# Imports
+import numpy as np
+from scipy.integrate import solve_bvp, solve_ivp
+from scipy.optimize import fsolve
+import matplotlib.pyplot as plt
+
+#
+# Alexandre Dufresne-Nappert
+# 20948586
+#
+
+# Define the Number of Nodes (n = x, m = y)
+n = 3
+m = 3
+totalNodes = n * m
+dx = dy = 0.1
+
+# Define the Boundary Condition Values
+T_left = 50
+T_bottom = 20
+
+#
+#
+# Solving for Sigma = 0
+#
+#
+
+#
+# Start Building the frame of the A Matrix
+#
+
+# Define the Middle Diagonal
+Amid = np.diag((-4) * np.ones(totalNodes))
+
+# Define the Diagonal 1 below and above
+Au1 = np.diag(np.ones(totalNodes - 1), 1)
+Ab1 = np.diag(np.ones(totalNodes - 1), -1)
+
+# Define the Diagonal 3 Below and Under
+Au3 = np.diag(np.ones(totalNodes - 3), 3)
+Ab3 = np.diag(np.ones(totalNodes - 3), -3)
+
+# Add Up everything
+A = Amid + Au1 + Ab1 + Au3 + Ab3
+
+# Make Appropriate Modifications due to Boundaries
+for i in range(3):
+    # Apply the Top Boundary Condition
+    A[6 + i, 3 + i] = 2
+
+    # Apply The Right Boundary Condition
+    A[2 + i * n, 1 + i * m] = 2
+
+# Leftovers from Boundary Conditions
+for i in range(2):
+    A[3 + i * n, 2 + i * n] = 0
+    A[2 + i * n, 3 + i * n] = 0
+
+#
+# Make the B Vector
+#
+
+# Empty b Vector
+b = np.zeros(totalNodes)
+
+# Apply Boundary Conditions
+b[:n] -= T_bottom
+b[::m] -= T_left
+
+# Solve the Matrix
+TSol = np.linalg.solve(A, b)
+
+# Create empty T Matrix
+T = np.zeros((4, 4))
+
+# Apply Boundary Conditions
+T[:, 0] = T_left
+T[-1, :] = T_bottom
+
+# Paste the Solution into the Matrix
+T[2, 1:] = TSol[:n]
+T[1, 1:] = TSol[n : 2 * n]
+T[0, 1:] = TSol[2 * n : 3 * n]
+
+# Display the Solution
+print("\nSigma = 0 Solution")
+print(T)
+
+#
+#
+# Solviong for Sigma = 1*10^-4
+#
+#
+
+# Define Unique values for this instance of the problem
+sigma = 1 * 10**-4
+Ta = 25
+
+# Create empty T Matrix
+T = np.zeros((4, 4))
+
+# Apply Boundary Conditions
+T[:, 0] = T_left
+T[-1, :] = T_bottom
+
+# Define a Counter for Max Iterations and the Error Matrix, Max Iterations alowed, a tolerance and a lamda for relaxation
+maxIter = 100
+k = 0
+eps = 100 * np.ones(T.shape)
+tol = 1 * 10**-8
+lam = 1.1
+
+# Iterate over Solving
+while k < maxIter and np.max(eps[:-1, 1:]) > tol:
+    for row in range(n - 1, -1, -1):
+        for col in range(1, m + 1):
+
+            # Store/Save the Previous Value
+            oldVal = T[row, col]
+
+            # Cover the Appriate Cases
+            if row == 0 and col == m:  # Top Right Corner
+                T[row, col] = 0.25 * (2 * T[row - 1, col]+ 2 * T[row, col - 1]+ dx**2 * sigma * (Ta - T[row, col] ** 4))
+            elif row == 0:  # Top Side
+                T[row, col] = 0.25 * (T[row - 1, col]+ T[row + 1, col]+ 2 * T[row, col - 1]+ dx**2 * sigma * (Ta - T[row, col] ** 4))
+            elif col == m:  # Right Side
+                T[row, col] = 0.25 * (2 * T[row - 1, col]+ T[row, col - 1]+ T[row, col - 1]+ dx**2 * sigma * (Ta - T[row, col] ** 4))
+            else:  # Any other case
+                T[row, col] = 0.25 * (T[row - 1, col] + T[row + 1, col]+ T[row, col - 1]+ T[row, col - 1]+ dx**2 * sigma * (Ta - T[row, col] ** 4))
+
+            # Apply some Relaxation
+            T[row, col] = lam * T[row, col] + (1 - lam) * oldVal
+
+            # Update the Error Matrix
+            eps[row, col] = np.abs((T[row, col] - oldVal) / T[row, col])
+    k += 1
+
+# Display the Resulting Solution
+print("\nSigma = 1-e4 Solution")
+print(T)
--- Assignments/Assignment 3/system_ODEs_question3.py
+++ Assignments/Assignment 3/system_ODEs_question3.py
@@ -14,60 +14,131 @@ import matplotlib.pyplot as plt
 #
 
 # Define Variables and Constants
-k = 10 #cm^3 / (mol * s)
-Da = 1 * 10**(-3) #cm^2/s
-Ca0 = 1 * 10**(-3) # mol/cm^3
+k = 10  # cm^3 / (mol * s)
+Da = 1 * 10 ** (-3)  # cm^2/s
+Ca0 = 1 * 10 ** (-3)  # mol/cm^3
 
 # Define the Span of X
 xInit = 0
-xEnd = 1.0 # cm
+xEnd = 1.0  # cm
 
-# Define the Number of Points and the Solution Vector
-n = 100
-x = np.linspace(0, 1, n)
+# Define Number of Points
+n = 500
+x = np.linspace(xInit, xEnd, n)
 
 # Define dx
-dx = (xEnd - xInit)/(n-1)
+dx = (xEnd - xInit) / (n - 1)
 
 # Create an Initial Guess Vector
 Cguess = np.ones(n) * Ca0
 
-def Matrix (CAs):
-    
+# Define a Function representing the System of Equations
+def Matrix(CAs, n, dx):
+
     # Initialize the System of Equations
     system = np.zeros(n)
-    
+
     # Apply first Boundary Conditions
     system[0] = CAs[0] - Ca0
-    
+
     # Iterate over the Internal Nodes
-    for i in range(1, n-1):
-        system[i] = CAs[i+1] - 2*CAs[i] + CAs[i-1] - ((k*dx**2)/Da)*(CAs[i]**2)
-    
+    for i in range(1, n - 1):
+        system[i] = (CAs[i + 1] - 2 * CAs[i] + CAs[i - 1] - ((k * dx**2) / Da) * (CAs[i] ** 2))
+
     # Apply Second Boundary Condition
-    system[n-1] = 2*CAs[i-1] - 2*CAs[i] - ((k*dx**2)/Da)*(CAs[i]**2)
-    
+    system[n - 1] = 2 * CAs[i - 1] - 2 * CAs[i] - ((k * dx**2) / Da) * (CAs[i] ** 2)
+
     # Return the System of Equations
     return system
+
+# Define a Function that solves Finite Differences with Varying n Value
+def SolveFiniteDiff (n):
+    # Define Number of Points
+    x = np.linspace(xInit, xEnd, n)
+
+    # Define dx
+    dx = (xEnd - xInit) / (n - 1)
+
+    # Create an Initial Guess Vector
+    Cguess = np.ones(n) * Ca0
     
-# Use FSolve to solve the System
-sol = fsolve(Matrix, Cguess)
+    Sol = fsolve(Matrix, Cguess, args=(n, dx))
+    
+    return [Sol, x]
 
+#finDifSol = fsolve(Matrix, Cguess, args=(n, dx))
+[finDifSol, x] = SolveFiniteDiff(n)
 
 #
 #
-# Copy Paste of the Question 2 Solution
+# Checking the convergence Method
+#
+#
+
+# ii) 
+# We can know that the solution has converged by increasing/doubling the number of nodes to be solved for in the finite difference method, and if error 
+# between the previous iteration and the current is close, we know we have converged upon a solution
+
+# Create a new Figure
+plt.figure(figsize=(18, 12))
+
+# Define the Number of Points and the Solution Vector
+nConv = 5
+
+# Define the Error Tolerance
+tol = 1*10**-3
+
+# Solve first Iteration
+[prevSol, prevX] = SolveFiniteDiff(nConv)
+
+# Plot the First Iteration
+plt.plot(prevX, prevSol, "--", label="Iter = 0")
+
+# Iterate through multiple Points
+for i in range(1, 10):
+    
+    # Double the Number of Points
+    nConv = nConv*2
+    
+    # Solve for the Doubled System
+    [newSol, xConv] = SolveFiniteDiff(nConv)
+    
+    # Plot our New Iteration
+    plt.plot(xConv, newSol, "--", label=f"Iter = {i}")
+
+    # Interpolate the number of Points so that we can compare to the new solution
+    inter = np.interp(xConv, prevX, prevSol)
+    
+    # Get the Error, if Max Error is below thershold we can break out of Loop
+    if np.max(np.abs((newSol - inter)/newSol)) < tol:
+        break
+    
+    # Save the new Solution as the Previous Values
+    prevX = xConv
+    prevSol = newSol
+
+# Show the Plot
+plt.legend()
+plt.title("Visual Display of Finite Difference Convergence")
+plt.xlabel("Distance into the Pore (cm)")
+plt.ylabel("Concentration of Species A (mol/cm^3)")
+plt.savefig("Q3_Convergence.png")
+plt.show()
+
+#
+#
+# Copy Paste of the Question 2 Solution (for comparing)
 #
 #
 
 # Define Variables and Constants
-k = 10 #cm^3 / (mol * s)
-Da = 1 * 10**(-3) #cm^2/s
-Ca0 = 1 * 10**(-3) # mol/cm^3
+k = 10  # cm^3 / (mol * s)
+Da = 1 * 10 ** (-3)  # cm^2/s
+Ca0 = 1 * 10 ** (-3)  # mol/cm^3
 
 # Define the Span of X
 xInit = 0
-xEnd = 1.0 # cm
+xEnd = 1.0  # cm
 
 # Define the Number of Points and the Arrays
 n = 100
@@ -75,28 +146,28 @@ xVals = np.linspace(xInit, xEnd, n)
 yVals = np.zeros((2, xVals.size))
 
 # Define the Differential Function
-def dfdx (x, f):
+def dfdx(x, f):
     # Decompose f into System of Equations
     [Ca, u] = f
-    
+
     # Return the System
-    return [u, 
-            (k*Ca**2)/Da]
+    return [u, (k * Ca**2) / Da]
 
 #
 # Solve BVP Method
 #
 
 # Define the Boundary Conditions
-def BCs (fa, fb):
-    
+def BCs(fa, fb):
+
     # Define the First Boundary Condition (Ca(0) = Ca0)
     BC1 = fa[0] - Ca0
-    
+
     # Define the Second Boundary Condition (dCa/dx (L) = 0)
     BC2 = fb[1]
-    
-    return [BC1,BC2]
+
+    return [BC1, BC2]
+
 
 # Solve using the BVP Method
 solBVP = solve_bvp(dfdx, BCs, xVals, yVals)
@@ -109,16 +180,15 @@ CaValsBVP = solBVP.y[0]
 #
 
 # Use Shooting Method Solver
-def Solver (uGuess):
+def Solver(uGuess):
     # Define the Boundaries
     CaInit = Ca0
-    
+
     # Solve for the Guess Using uGuess
     yGuess = solve_ivp(dfdx, (xInit, xEnd), [CaInit, uGuess[0]])
-    
+
     # Return the Last Value that we Are trying to solve for
     return yGuess.y[1, -1]
-
 
 # Make an Initial Guess (Magic Number)
 uGuessInit = [-0.001]
@@ -136,15 +206,15 @@ solIVP = solve_ivp(dfdx, (0, 1), [Ca0, correctU[0]], t_eval=xVals)
 CaValsIVP = solIVP.y[0]
 
 #
-# Plot the Results
+# Plot the Results (iii)
 #
 plt.figure(figsize=(18, 12))
+plt.plot(x, finDifSol, "o", label="Finite Difference")
 plt.plot(solBVP.x, CaValsBVP, "b-", label="BVP")
-plt.plot(solIVP.t, CaValsIVP, "r--" , label="IVP")
+plt.plot(solIVP.t, CaValsIVP, "r--", label="IVP")
 plt.xlabel("Distance into the Pore (cm)")
 plt.ylabel("Concentration of Species A (mol/cm^3)")
 plt.title("Solution to the Concentration of Species A diffused in a Cylindrical Pore")
-plt.plot(x, sol, "o", label="Finite Difference")
 plt.legend()
 plt.savefig("Q3_FiniteDiff_Graph")
 plt.show()
